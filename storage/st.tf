11e3d336eef6b49933567048926f728d6bf13758
Andrea Rizzioli <andrea.rizzioli@barnett-waddingham.co.uk>
2023-11-28 15:18:07 +0000

    Renamed resource

--- storage/st.tf
+++ storage/st.tf
@@ -1,4 +1,4 @@
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "st_acct" {
   name                     = var.st_name
   resource_group_name      = var.st_rg_name
   location                 = var.st_location
