0ae9ba68f84f8baea9d6e6f981682c0239885b08
Andrea Rizzioli <andrea.rizzioli@barnett-waddingham.co.uk>
2023-12-06 12:29:52 +0000

    renamed priv dns network link

--- private_dns_zone/private_dns_zone_link.tf
+++ private_dns_zone/private_dns_zone_link.tf
@@ -1,4 +1,4 @@
-resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_aks_dns_zone_link" {
+resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_blob_dns_zone_link" {
   name                  = "${var.private_dns_zone_name}_link_westeurope"
   resource_group_name   = var.private_dns_zone_rg_name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone.name
