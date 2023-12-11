36341d7e69b38bff07625399feeb4189149f775f
Andrea Rizzioli <andrea.rizzioli@barnett-waddingham.co.uk>
2023-12-11 14:18:07 +0000

    Added private dns resolver inbound endpoint

--- private_dns_resolver/private_dns_resolver.tf
+++ private_dns_resolver/private_dns_resolver.tf
@@ -5,3 +5,14 @@ resource "azurerm_private_dns_resolver" "private_dns_resolver" {
   virtual_network_id  = var.private_dns_resolver_virtual_network_id
   tags                = var.private_dns_resolver_tags
 }
+resource "azurerm_private_dns_resolver_inbound_endpoint" "private_dns_resolver_inbound_endpoint" {
+  name                    = var.private_dns_resolver_inbound_endpoint_name
+  private_dns_resolver_id = azurerm_private_dns_resolver.private_dns_resolver.id
+  location                = var.private_dns_resolver_location
+  tags                    = {}
+
+  ip_configurations {
+    private_ip_allocation_method = var.private_dns_resolver_private_ip_allocation_method
+    subnet_id                    = var.private_dns_resolver_private_in_ep_subnet_id
+  }
+}
--- private_dns_resolver/variables.tf
+++ private_dns_resolver/variables.tf
@@ -15,9 +15,25 @@ variable "private_dns_resolver_location" {
 }
 variable "private_dns_resolver_virtual_network_id" {
   type        = string
-  default     = "westeurope"
+  default     = "/subscriptions/xx/resourceGroups/rg-xx/providers/Microsoft.Network/virtualNetworks/vnet-xx"
   description = "private dns resolver network id"
 }
+variable "private_dns_resolver_inbound_endpoint_name" {
+  type        = string
+  default     = "arr_priv_dns_in_ep_name"
+  description = "private dns resolver inbound endpoint name"
+}
+variable "private_dns_resolver_private_ip_allocation_method" {
+  type        = string
+  default     = "Dynamic"
+  description = "private dns resolver private ip allocation method"
+}
+variable "private_dns_resolver_private_in_ep_subnet_id" {
+  type        = string
+  default     = "Dynamic"
+  description = "private dns resolver inbound endpoint subnet id"
+}
+                  
 variable "private_dns_resolver_tags" {
   type = map(string)
   default = {
