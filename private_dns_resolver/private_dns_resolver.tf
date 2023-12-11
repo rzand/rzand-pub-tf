b3a7cea1e8d1257c1b8c1b8ca2d6bcd2351fc948
Andrea Rizzioli <andrea.rizzioli@barnett-waddingham.co.uk>
2023-12-11 17:27:17 +0000

    Added forwarding rule

--- private_dns_resolver/private_dns_resolver.tf
+++ private_dns_resolver/private_dns_resolver.tf
@@ -64,3 +64,33 @@ output "forwarding_rules" {
     ]
   ])
 }
+locals {
+  private_dns_resolver_forwarding_rules =  flatten([
+    for ruleset_name, ruleset in var.private_dns_resolver_forwarding_rulesets :
+    [
+      for rule in ruleset.forwarding_rules :
+      {
+        forwarding_ruleset_name = ruleset_name
+        forwarding_rule_name    = rule.name
+        domain_name             = rule.domain_name
+        target_dns_servers      = rule.target_dns_servers
+      }
+    ]
+  ])
+}
+resource "azurerm_private_dns_resolver_forwarding_rule" "forwarding_rule" {
+  count = length(local.private_dns_resolver_forwarding_rules)
+
+  name                      = local.private_dns_resolver_forwarding_rules[count.index].forwarding_rule_name
+  dns_forwarding_ruleset_id = lookup(azurerm_private_dns_resolver_dns_forwarding_ruleset.forwarding_ruleset, local.private_dns_resolver_forwarding_rules[count.index].forwarding_ruleset_name, null).id
+  domain_name               = local.private_dns_resolver_forwarding_rules[count.index].domain_name
+  enabled                   = true
+
+  dynamic "target_dns_servers" {
+    for_each = local.private_dns_resolver_forwarding_rules[count.index].target_dns_servers
+    content {
+      ip_address = target_dns_servers.value.ip_address
+      port       = target_dns_servers.value.port
+    }
+  }
+}
